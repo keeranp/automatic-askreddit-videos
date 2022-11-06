7273d95b5b3ca92247d45ba03598c3b80a93f1ba
keeranp <keeran.paheerathan@gmail.com>
2022-11-06 17:06:02 +0100

    Formated data

--- retrievedata.py
+++ retrievedata.py
@@ -10,5 +10,19 @@ reddit = praw.Reddit(
     user_agent=os.environ["USER_AGENT"],
 )
 
-for submission in reddit.subreddit("TooAfraidToAsk").hot(limit=10):
-    print(submission.title)
+contents = []
+
+for submission in reddit.subreddit("TooAfraidToAsk+AskReddit").hot(limit=10):
+    content = {}
+    content["url"] = submission.url
+    content["title"] = submission.title
+    content["comments"] = []
+    print(len(submission.comments))
+    for i in range(min(10, len(submission.comments))):
+        content["comments"].append({
+            "url": submission.comments[i].permalink,
+            "id": submission.comments[i].id,
+            "body": submission.comments[i].body,
+        })
+
+    # print(content)
