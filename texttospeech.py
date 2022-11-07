09e3e0369f87f6a0ba6a58a7a5f778869c0ce12e
keeranp <keeran.paheerathan@gmail.com>
2022-11-07 13:57:38 +0100

    Added sounds length to the content object

--- texttospeech.py
+++ texttospeech.py
@@ -8,18 +8,19 @@ MAX_LENGTH = 50
 def text_to_speech(content):
     print("Turning text to speech")
 
-
     length = 0
     title_sound = gTTS(content["title"], lang="en", slow=False)
-    title_sound_path = os.path.join("assets","sounds", "title.mp3")
+    title_sound_path = os.path.join("assets", "sounds", "title.mp3")
     title_sound.save(title_sound_path)
+    content["title_length"] = MP3(title_sound_path).info.length
     length += MP3(title_sound_path).info.length
 
     i = 0
 
     while length < MAX_LENGTH and i < len(content["comments"]):
         tts = gTTS(content["comments"][i]["body"], lang="en", slow=False)
-        path = os.path.join("assets","sounds", content["comments"][i]["id"] + ".mp3")
+        path = os.path.join("assets", "sounds", content["comments"][i]["id"] + ".mp3")
         tts.save(path)
+        content["comments"][i]["sound_length"] = MP3(path).info.length
         length += MP3(path).info.length
         i += 1
